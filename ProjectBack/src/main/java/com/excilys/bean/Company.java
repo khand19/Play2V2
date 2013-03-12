33f69dabf8a738773ca565241fe59c2debc98aef
klaurent <klaurent@desktop-0168.ebi.excilys.com>
2013-03-12 15:56:50 +0100

    test

--- ProjectBack/src/main/java/com/excilys/bean/Company.java
+++ ProjectBack/src/main/java/com/excilys/bean/Company.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+ 
 @Entity
 @Table(name = "company")
 public class Company {
